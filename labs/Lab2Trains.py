76839be0b34fa7326229802b08283a8af7f147c3
Olga Knutova <G00425661@atu.ie>
2024-12-29 13:53:09 +0000

    audio file

--- labs/Lab2Trains.py
+++ labs/Lab2Trains.py
@@ -25,25 +25,26 @@ with open("week02_train.csv",mode="w", newline='') as train_file:
     train_writer = csv.writer(train_file, delimiter='\t',quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
     objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
-    """dataList=[]
+    dataList=[]
     for retriveTag in retrieveTags:
         datanode = objTrainPositionsNodes.getElementsByTagName(retriveTag).item(0)
-        dataList.append(datanode.firstChild.nodeValue.strip())"""
+        dataList.append(datanode.firstChild.nodeValue.strip())
 
     for objTrainPositionsNode in objTrainPositionsNodes:
         traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0)
         traincode = traincodenode.firstChild.nodeValue.strip()
 
         if traincode.startswith("E"):
-            #print (traincode)
+            print (traincode)
             dataList = []
             for retriveTag in retrieveTags:
                 datanode=objTrainPositionsNode.getElementsByTagName(retriveTag).item(0)
                 dataList.append(datanode.firstChild.nodeValue.strip())
             train_writer.writerow(dataList)
 
-"""objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
+objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
 for objTrainPositionsNodes in objTrainPositionsNodes:
     trainlatitudenode = objTrainPositionsNodes.getElementsByTagName("TrainLatitude").item(0)
     trainlatitude = trainlatitudenode.firstChild.nodeValue.strip()
-    #print (trainlatitude)"""
+    print (trainlatitude)
+    
